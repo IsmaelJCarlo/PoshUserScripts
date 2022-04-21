13d82884e8ad03e3725ae38fa6c43d664a0653e5
IsmaelJCarlo <7817004+IsmaelJCarlo@users.noreply.github.com>
2022-04-21 08:10:37 -0500

    Added a log file

--- Cleanup-Downloads.ps1
+++ Cleanup-Downloads.ps1
@@ -1,2 +1,2 @@
 # Cleans up files oder than 5 days in Downloads Folder for Current user 
-ls ~\Downloads | where {($_.LastWriteTime).AddDays(5) -lt (get-date)} | remove-item -force -recurse
+ls ~\Downloads | where {($_.LastWriteTime).AddDays(4) -lt (get-date)} | remove-item -force -recurse -verbose  | Out-File -FilePath ~\Downloads\Cleanup-Downloads.log
